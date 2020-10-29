12780d68dfc3f6b533e89818539bca674f3478fe
naing-lin-oo <73457446+naing-lin-oo@users.noreply.github.com>
2020-10-29 23:52:29 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,11 +2,6 @@
 
 echo "Sending Email";
 echo "Another Email Sent";
-echo "Third Line From CLI";                    
-echo "Third Line From Online";
-echo "fourth Line From CLI";
-echo "Fourth Line From Online";
-echo "Fifth Line From Online";
-echo "Fifth Line From CLI";
+
 
 ?>
